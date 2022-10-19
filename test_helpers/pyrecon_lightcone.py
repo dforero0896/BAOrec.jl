e5c8d694a52154d667358f0ec24750e657c7a77e
Daniel Forero <daniel.forerosanchez@epfl.ch>
2022-10-19 04:56:56 -0700

    Added benchmarks for pyrecon. and pyrecon MG plots. Pyrecon takes ~1e1s for reconstruction ans similarly for reading from fields.

--- test_helpers/pyrecon_lightcone.py
+++ test_helpers/pyrecon_lightcone.py
@@ -14,8 +14,9 @@ import numpy as np
 import sys
 sys.path.append("/global/u1/d/dforero/codes/powspec_py/powspec")
 from pypowspec import compute_auto_lc
-from pyrecon import IterativeFFTReconstruction
+from pyrecon import IterativeFFTReconstruction, MultiGridReconstruction
 from pyrecon.utils import sky_to_cartesian, cartesian_to_sky
+import time
 
 
 
@@ -66,19 +67,21 @@ if __name__ == '__main__':
     boxsize = max(box_max - box_min)
     boxcenter = 0.5 * (rand[:,:3].max(axis=0) + rand[:,:3].min(axis=0))
     
-    recon = IterativeFFTReconstruction(f=0.757, bias=2.2, nmesh=512, nthreads=64, fft_engine = 'fftw', boxsize = boxsize, boxcenter = boxcenter, wrap=False)
+    recon = IterativeFFTReconstruction(f=0.757, bias=2.2, nmesh=512, nthreads = 128, fft_engine = 'fftw', boxsize = boxsize, boxcenter = boxcenter, wrap=False)
+    s = time.time()
     recon.assign_data(data[:,:3], weights = fkp_data)
     recon.assign_randoms(rand[:,:3], weights = fkp_rand)
-    
-    
     recon.set_density_contrast(smoothing_radius = 15.)
-    
     recon.run(niterations = 3)
+    print(f"Iterative recon took {time.time() - s}s", flush=True)
     
     
+    
+    s = time.time()
     positions_rec_data = recon.read_shifted_positions(data[:,:3]).astype(np.double)
     positions_rec_randoms_sym = recon.read_shifted_positions(rand[:,:3]).astype(np.double) #Sym
     positions_rec_randoms_iso = recon.read_shifted_positions(rand[:,:3], field='disp').astype(np.double) #Iso
+    print(f"Reading from field took {time.time() - s}s", flush=True)
     r, ra, dec = cartesian_to_sky(data[:,:3])
     data = np.c_[ra, dec, np.interp(r, rtable, ztable), data[:,3]]
     r, ra, dec = cartesian_to_sky(rand[:,:3])
@@ -126,3 +129,85 @@ if __name__ == '__main__':
     ax.legend(loc='top')
 
     fig.savefig("plots/pyrecon_lightcone.png")
+
+
+
+    fig, ax = pplt.subplots(nrows = 1,ncols = 3, share=0)
+    
+    data = pd.read_csv(data_fn, delim_whitespace = True, engine = 'c', usecols = (0,1,3,4), names = ['ra', 'dec', 'z', 'nz']).values
+    data = data[(data[:,2] > 0.8) & (data[:,2] < 1)]
+
+    rand = pd.read_csv(rand_fn, delim_whitespace = True, engine = 'c', usecols = (0,1,3,4), names = ['ra', 'dec', 'z', 'nz']).values
+    rand = rand[(rand[:,2] > 0.8) & (rand[:,2] < 1)]
+    
+    _data = data.copy()
+    _rand = data.copy()
+    
+    data = np.c_[sky_to_cartesian(np.interp(data[:,2], ztable, rtable) , data[:,0], data[:,1]), data[:,3]]
+    rand = np.c_[sky_to_cartesian(np.interp(rand[:,2], ztable, rtable) , rand[:,0], rand[:,1]), rand[:,3]]
+
+    fkp_data = 1. / (1 + data[:,3] * P0)
+    fkp_rand = 1. / (1 + rand[:,3] * P0)
+    recon = MultiGridReconstruction(f=0.757, bias=2.2, nmesh=512, nthreads = 128, fft_engine = 'fftw', boxsize = boxsize, boxcenter = boxcenter, wrap=False)
+    s = time.time()
+    recon.assign_data(data[:,:3], weights = fkp_data)
+    recon.assign_randoms(rand[:,:3], weights = fkp_rand)
+    recon.set_density_contrast(smoothing_radius = 15.)
+    recon.run()
+    print(f"Multigrid recon took {time.time() - s}s", flush=True)
+    
+    
+    
+    s = time.time()
+    positions_rec_data = recon.read_shifted_positions(data[:,:3]).astype(np.double)
+    positions_rec_randoms_sym = recon.read_shifted_positions(rand[:,:3]).astype(np.double) #Sym
+    positions_rec_randoms_iso = recon.read_shifted_positions(rand[:,:3], field='disp').astype(np.double) #Iso
+    print(f"Reading from field took {time.time() - s}s", flush=True)
+
+    r, ra, dec = cartesian_to_sky(data[:,:3])
+    data = np.c_[ra, dec, np.interp(r, rtable, ztable), data[:,3]]
+    r, ra, dec = cartesian_to_sky(rand[:,:3])
+    rand = np.c_[ra, dec, np.interp(r, rtable, ztable), rand[:,3]]
+
+    pk = compute_auto_lc(data[:,0], data[:,1], data[:,2], np.ones_like(data[:,3]), fkp_data, data[:,3],
+                    rand[:,0], rand[:,1], rand[:,2], np.ones_like(rand[:,3]), fkp_rand, rand[:,3],
+                    powspec_conf_file = "test_helpers/powspec_lc.conf",
+                    output_file = None)
+    plot_correlations(pk, ax, label = 'Pre')
+
+    fig.savefig("plots/pyrecon_lightcone_mg.png")
+
+    
+    r, ra, dec = cartesian_to_sky(positions_rec_data)
+    
+    data = np.c_[ra, dec, np.interp(r, rtable, ztable), data[:,3]]
+    r, ra, dec = cartesian_to_sky(positions_rec_randoms_iso)
+    rand = np.c_[ra, dec, np.interp(r, rtable, ztable), rand[:,3]]
+
+    fkp_data = 1. / (1 + data[:,3] * P0)
+    fkp_rand = 1. / (1 + rand[:,3] * P0)
+
+    pk = compute_auto_lc(data[:,0], data[:,1], data[:,2], np.ones_like(data[:,3]), fkp_data, data[:,3],
+                    rand[:,0], rand[:,1], rand[:,2], np.ones_like(rand[:,3]), fkp_rand, rand[:,3],
+                    powspec_conf_file = "test_helpers/powspec_lc.conf",
+                    output_file = None)
+
+    plot_correlations(pk, ax, label = 'Iso')
+    
+
+    fig.savefig("plots/pyrecon_lightcone_mg.png")
+    
+    r, ra, dec = cartesian_to_sky(positions_rec_randoms_sym)
+    rand = np.c_[ra, dec, np.interp(r, rtable, ztable), rand[:,3]]
+    fkp_data = 1. / (1 + data[:,3] * P0)
+    fkp_rand = 1. / (1 + rand[:,3] * P0)
+
+    pk = compute_auto_lc(data[:,0], data[:,1], data[:,2], np.ones_like(data[:,3]), fkp_data, data[:,3],
+                    rand[:,0], rand[:,1], rand[:,2], np.ones_like(rand[:,3]), fkp_rand, rand[:,3],
+                    powspec_conf_file = "test_helpers/powspec_lc.conf",
+                    output_file = None)
+    plot_correlations(pk, ax, label = 'Sym')
+
+    ax.legend(loc='top')
+
+    fig.savefig("plots/pyrecon_lightcone_mg.png")
--- test_helpers/pyrecon_simulation.py
+++ test_helpers/pyrecon_simulation.py
@@ -9,10 +9,10 @@ import proplot as pplt
 from tqdm import tqdm
 import glob
 import sys
-from pyrecon import IterativeFFTReconstruction
+from pyrecon import IterativeFFTReconstruction, MultiGridReconstruction
 sys.path.append("/global/u1/d/dforero/codes/powspec_py/powspec")
 from pypowspec import compute_auto_box, compute_auto_box_rand
-
+import time
 
 
 def plot_correlations(pk_res, ax, label):
@@ -39,17 +39,21 @@ if __name__ == '__main__':
     rand_multiplier = 10
     ndata = pre.shape[0]
 
-    recon = IterativeFFTReconstruction(f=0.757, bias=2.2, nmesh=512, boxsize=box_size, boxcenter=box_size / 2, los=[0,0,1], wrap=True, nthreads=64, fft_engine = 'fftw')
+    recon = IterativeFFTReconstruction(f=0.757, bias=2.2, nmesh=512, boxsize=box_size, boxcenter=box_size / 2, los=[0,0,1], wrap=True, nthreads = 128, fft_engine = 'fftw')
+    s = time.time()
     recon.assign_data(pre, np.broadcast_to([1.], pre.shape[0]))
     recon.set_density_contrast(smoothing_radius = 15.)
+    recon.run(niterations = 3)
+    print(f"Iterative recon took {time.time() - s}s", flush=True)
           
     
-    recon.run(niterations = 3)
+    s = time.time()
     positions_rec_data = recon.read_shifted_positions(pre).astype(np.double)
     rng = np.random.default_rng(42)
     positions_randoms = rng.random((rand_multiplier * ndata, 3), dtype = np.float32) * box_size
     positions_rec_randoms_sym = recon.read_shifted_positions(positions_randoms).astype(np.double) #Sym
     positions_rec_randoms_iso = recon.read_shifted_positions(positions_randoms, field='disp').astype(np.double) #Iso
+    print(f"Reading from field took {time.time() - s}s", flush=True)
     
 
     
@@ -88,3 +92,59 @@ if __name__ == '__main__':
     ax.legend(loc='top')
 
     fig.savefig("plots/pyrecon_simulation.png")
+
+
+
+    recon = MultiGridReconstruction(f=0.757, bias=2.2, nmesh=512, boxsize=box_size, boxcenter=box_size / 2, los=[0,0,1], wrap=True, nthreads = 128, fft_engine = 'fftw')
+    s = time.time()
+    recon.assign_data(pre, np.broadcast_to([1.], pre.shape[0]))
+    recon.set_density_contrast(smoothing_radius = 15.)
+    recon.run()
+    print(f"Multigrid recon took {time.time() - s}s", flush=True)
+          
+    
+    s = time.time()
+    positions_rec_data = recon.read_shifted_positions(pre).astype(np.double)
+    rng = np.random.default_rng(42)
+    positions_randoms = rng.random((rand_multiplier * ndata, 3), dtype = np.float32) * box_size
+    positions_rec_randoms_sym = recon.read_shifted_positions(positions_randoms).astype(np.double) #Sym
+    positions_rec_randoms_iso = recon.read_shifted_positions(positions_randoms, field='disp').astype(np.double) #Iso
+    print(f"Reading from field took {time.time() - s}s", flush=True)
+    
+
+    
+    fig, ax = pplt.subplots(nrows = 1,ncols = 3, share=0)
+    pre = (pre + box_size) % box_size
+    pk = compute_auto_box(pre[:,0], pre[:,1], pre[:,2], np.ones_like(pre[:,0]), 
+                      powspec_conf_file = "test_helpers/powspec_auto.conf",
+                      output_file = None)
+    plot_correlations(pk, ax, label = 'Pre')
+
+    fig.savefig("plots/pyrecon_simulation_mg.png")
+
+    
+
+    post_d = positions_rec_data
+    post_r = positions_rec_randoms_iso
+    post_d = (post_d + box_size) % box_size
+    post_r = (post_r + box_size) % box_size
+    pk = compute_auto_box_rand(post_d[:,0], post_d[:,1], post_d[:,2], np.ones_like(post_d[:,0]),
+                      post_r[:,0], post_r[:,1], post_r[:,2], np.ones_like(post_r[:,0]),
+                      powspec_conf_file = "test_helpers/powspec_auto.conf",
+                      output_file = None)    
+    plot_correlations(pk, ax, label = 'Iso')
+    
+
+    fig.savefig("plots/pyrecon_simulation_mg.png")
+    
+    post_r = positions_rec_randoms_sym
+    post_r = (post_r + box_size) % box_size
+    pk = compute_auto_box_rand(post_d[:,0], post_d[:,1], post_d[:,2], np.ones_like(post_d[:,0]),
+                      post_r[:,0], post_r[:,1], post_r[:,2], np.ones_like(post_r[:,0]),
+                      powspec_conf_file = "test_helpers/powspec_auto.conf",
+                      output_file = None)    
+    plot_correlations(pk, ax, label = 'Sym')
+
+    ax.legend(loc='top')
+
+    fig.savefig("plots/pyrecon_simulation_mg.png")
